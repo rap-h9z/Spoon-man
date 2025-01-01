762626b6654d42398aaa1dd964dedeb05e651a6f
raha <rahapazoki0@gmail.com>
2025-01-01 21:09:50 +0330

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by raha pazoki on 10/12/1403 AP.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

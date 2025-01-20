5f61feb836e2eb5399e0c44e5a0cee625ec211bd
raha <rahapazoki0@gmail.com>
2025-01-20 20:52:37 +0330

    new*

--- project/main.cpp
+++ project/main.cpp
@@ -7,6 +7,11 @@ using namespace std;
 using namespace std::chrono;
 #define row 20
 #define col 20
+struct score{
+    int movements=0;
+    int countB=0;
+    long int time=0;
+};
 int main()
 {
     int hardness();
@@ -20,7 +25,7 @@ int main()
     void easygame();
     void mediumgame();
     void hardgame();
-    int ans;
+    void menu();
     
     string s="Welcome to the Spoon Man game";
     for (int i=0; i<30; ++i) {
@@ -28,88 +33,31 @@ int main()
         cout<<s[i];
     }
     usleep(2000000);
-    string t="\n\n1.Start\n2.Hardness level\n3.Help\n4.Exit";
-    for (int j=0; j<41; ++j) {
-        usleep(100000);
-        cout<<t[j];
-    }
-    cin>>ans;
-    switch (ans) {
-        case 1:
-            easygame();
-            break;
-        case 2:
-            switch (hardness()) {
-                case 1:
-                    cout<<"\n\n\n\n\n\n\n\n\n\n1.Start\n2.back";
-                    cin>>ans;
-                    switch (ans) {
-                        case 1:
-                            easygame();
-                            break;
-                        default:
-                            hardness();
-                            break;
-                    }
-                    break;                case 2:
-                    cout<<"\n\n\n\n\n\n\n\n\n\n1.Start\n2.back";
-                    cin>>ans;
-                    switch (ans) {
-                        case 1:
-                            mediumgame();
-                            break;
-                        default:
-                            hardness();
-                            break;
-                    }
-                    break;
-                case 3:
-                    cout<<"\n\n1.Start\n2.back";
-                    cin>>ans;
-                    switch (ans) {
-                        case 1:
-                            hardgame();
-                            break;
-                        default:
-                            hardness();
-                            break;
-                    }
-                default:
-                    cout<<"\n\n1.Start\n2.Hardness level\n3.Help\n4.Exit";
-                    cin>>ans;
-                    switch (ans) {
-                        case 1:
-                            easygame();
-                            break;
-                        case 2:
-                            hardness();
-                        case 3:
-                            help();
-                        default:
-                            break;
-                    }
-                    break;
-            }
-            break;
-        case 3:
-            help();
-            break;
-        default:
-            break;
-    }
+    menu();
+    return 0;
+}
+double point( score m, score cb, score t,const int wm,const int wt,const int wb)
+{
+    double s;
+    s=1000/(1+(m.movements*wm)+(cb.countB*wb)+(t.time*wt));
+    return s;
 }
 int hardness()
 {
     int ans;
-    cout<<"\n\n1.easy"<<"\n2.medium"<<"\n3.Hard"<<"\n4.back";
+    string k="\n\n1.easy\n2.medium\n3.Hard\n4.menu\n";
+    for (int i=0; i<33; ++i) {
+        usleep(200000);
+        cout<<k[i];
+    }
     cin>>ans;
     return ans;
 }
 void help()
 {
-    string i="Hi\nIn this game if you want to survive,you just need to kill enemis with bombs\n you can move with D:⮕ ,W:⬆ ,S:⬇ ,A:⬅\n\nS:you\nE:your enemy\nXX:unbreakable wall\n= :breakable wall\nB!:bomb\n\n\nGame maker:Raha Pazoki\nId:4021226411";
-    for (int k=0; k<230; ++k) {
-        usleep(200000);
+    string i="\n\n\n\n\nHi\nIn this game if you want to survive,you just need to kill enemis with bombs and finally reach home.\n you can move with D:⮕ ,W:⬆ ,S:⬇ ,A:⬅ and press B button whenever you needed to demolish something.\n\nS:you\nE:your enemy\nXX:unbreakable wall\n= :breakable wall\nB!:bomb\n<>:home\n\n\nGame maker:Raha Pazoki\nId:4021226411";
+    for (int k=0; k<328; ++k) {
+        usleep(100000);
         cout<<i[k];
     }
 }
@@ -188,14 +136,18 @@ void easyB(char ground[row][col],int x,int y)
 }
 void easygame()
 {
-    auto start=high_resolution_clock::now();
-    long int T;
+    score m,t,cb;
+    void menu();
+    const int wm=1,wb=2,wt=1;
     char ground[row][col];
     int x=2,y=2;
     char ans;
     int count=0;
+    int countB=0;
+    auto start=high_resolution_clock::now();
     easyplayground(ground,x,y);
-        cout<<"Go..."<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    while (x!=18||y!=17) {
+        cout<<"Go..."<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
         cin>>ans;
         switch (ans) {
             case 'w':
@@ -220,15 +172,31 @@ void easygame()
                 break;
             case 'b':
                 easyB(ground,x,y);
+                ++countB;
                 break;
             case 'q':
-                return;
+                string k="Game over";
+                for (int j=0; j<9; ++j) {
+                    usleep(200000);
+                    cout<<k[j];
+                }
+                sleep(2);
+                menu();
+                break;
         }
-    ++count;
+        ++count;
+    }
+    m.movements=count;
+    cb.countB=countB;
     auto end=high_resolution_clock::now();
     auto duration= duration_cast<seconds>(end-start);
-    T=duration.count();
-    cout<<T;
+    t.time=duration.count();
+    string s="\n\ncongrats...you win!\nyour point:";
+    for (int i=0; i<34; ++i) {
+        usleep(100000);
+        cout<<s[i];
+    }
+    cout<<point(m,cb,t,wm,wt,wb)<<endl;
     
 }
 void mediumplayground (char ground[row][col],int x,int y)
@@ -251,7 +219,7 @@ void mediumplayground (char ground[row][col],int x,int y)
                 cout<<" "<<ground[x][y];
             else {
                 ran=(rand()%20);
-                if ((ran%80==0)){
+                if ((ran%50==0)){
                     ground[i][j]='E';
                     cout<<ground[i][j]<<" ";}
                 else if(ran%7==0){
@@ -291,7 +259,7 @@ void mediumB (char ground[row][col],int x,int y)
             }
             else {
                 ran=(rand()%20);
-                if ((ran%80==0)){
+                if ((ran%50==0)){
                     ground[i][j]='E';
                     cout<<ground[i][j]<<" ";}
                 else if(ran%7==0){
@@ -306,14 +274,18 @@ void mediumB (char ground[row][col],int x,int y)
 }
 void mediumgame()
 {
-    auto start=high_resolution_clock::now();
-    long int T;
+    score m,t,cb;
+    void menu();
+    const int wm=2,wb=2,wt=1;
     char ground[row][col];
     int x=2,y=2;
     char ans;
     int count=0;
+    int countB=0;
+    auto start=high_resolution_clock::now();
     mediumplayground(ground,x,y);
-        cout<<"Go..."<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    while (x!=18||y!=17){
+        cout<<"Go..."<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
         cin>>ans;
         switch (ans) {
             case 'w':
@@ -338,17 +310,33 @@ void mediumgame()
                 break;
             case 'b':
                 mediumB(ground,x,y);
+                ++countB;
                 break;
             case 'q':
-                return;
+                string k="Game over";
+                for (int j=0; j<8; ++j) {
+                    usleep(200000);
+                    cout<<k[j];
+                }
+                sleep(2);
+                menu();
+                break;
         }
-    ++count;
+        ++count;
+    }
+    m.movements=count;
+    cb.countB=countB;
     auto end=high_resolution_clock::now();
     auto duration= duration_cast<seconds>(end-start);
-    T=duration.count();
-    cout<<T;
-    
-}void hardplayground ( char ground[row][col],int x,int y)
+    t.time=duration.count();
+    string s="\n\ncongrats...you win!\nyour point:";
+    for (int i=0; i<34; ++i) {
+        usleep(100000);
+        cout<<s[i];
+    }
+    cout<<point(m,cb,t,wm,wt,wb)<<endl;
+}
+void hardplayground ( char ground[row][col],int x,int y)
 {
     ground[x][y] = 'S';
     int ran;
@@ -423,14 +411,18 @@ void hardB( char ground[row][col],int x,int y)
 }
 void hardgame()
 {
-    auto start=high_resolution_clock::now();
-    long int T;
+    score m,t,cb;
+    void menu();
+    const int wm=2,wb=3,wt=2;
     char ground[row][col];
     int x=2,y=2;
     char ans;
     int count=0;
+    int countB=0;
+    auto start=high_resolution_clock::now();
     hardplayground(ground,x,y);
-        cout<<"Go..."<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    while (x!=18||y!=17){
+        cout<<"Go..."<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
         cin>>ans;
         switch (ans) {
             case 'w':
@@ -455,14 +447,90 @@ void hardgame()
                 break;
             case 'b':
                 hardB(ground,x,y);
+                ++countB;
                 break;
             case 'q':
-                return;
+                string k="Game over";
+                for (int j=0; j<8; ++j) {
+                    usleep(200000);
+                    cout<<k[j];
+                }
+                sleep(2);
+                menu();
+                break;
         }
+    }
     ++count;
     auto end=high_resolution_clock::now();
     auto duration= duration_cast<seconds>(end-start);
-    T=duration.count();
-    cout<<T;
-    
+    t.time=duration.count();
+    string s="\n\ncongrats...you win!\nyour point:";
+    for (int i=0; i<34; ++i) {
+        usleep(100000);
+        cout<<s[i];
+    }
+    cout<<point(m, cb, t, wm, wt, wb);
+}
+void menu()
+{
+    int ans;
+    string t="\n\n1.Start\n2.Hardness level\n3.Help\n4.Exit\n";
+    for (int j=0; j<41; ++j) {
+        usleep(100000);
+        cout<<t[j];
+    }
+    cin>>ans;
+    switch (ans) {
+        case 1:
+            easygame();
+            break;
+        case 2:
+            switch (hardness()) {
+                case 1:
+                    cout<<"\n\n1.start\n2.menu";
+                    cin>>ans;
+                    switch (ans) {
+                        case 1:
+                            easygame();
+                            break;
+                        case 2:
+                            menu();
+                            break;
+                    }
+                    break;
+                case 2:
+                    cout<<"\n\n1.start\n2.menu";
+                    cin>>ans;
+                    switch (ans) {
+                        case 1:
+                            mediumgame();
+                            break;
+                        case 2:
+                            menu();
+                            break;
+                    }
+                case 3:
+                    cout<<"\n\n1.start\n2.menu";
+                    cin>>ans;
+                    switch (ans) {
+                        case 1:
+                            hardgame();
+                            break;
+                        case 2:
+                            menu();
+                            break;
+                    }
+                    break;
+                case 4:
+                    menu();
+                    break;
+            }
+            break;
+        case 3:
+            help();
+            menu();
+            break;
+        case 4:
+            break;
+    }
 }
